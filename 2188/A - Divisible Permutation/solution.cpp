7d6ec79b0ff0907e37687503b6c1bf3093fbd95c
Vaibhav rattan Jha <vrj28012007@gmail.com>
2026-08-25 12:51:31 +0530

    Add Divisible Permutation [A] from Codeforces

---
+++ 2188/A - Divisible Permutation/solution.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        int x=n,y=1;
+        for(int i=n-1;i>=0;i--){
+            a[i]=x;
+            a[i-1]=y;
+            x--;
+            y++;
+            i--;
+        }
+        for(int i=0;i<n;i++) cout<<a[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
